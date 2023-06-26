efce171776fb0155b535c0d8a56711eda6d676d1
Hovhannes <hovhannes.mkoyan@gmail.com>
2023-06-26 22:52:08 +0200

    feat: adjust products page

--- src/components/GuestHomePage/LogoCloud.tsx
+++ src/components/GuestHomePage/LogoCloud.tsx
@@ -2,26 +2,38 @@ import Image from "next/image";
 
 export default function LogoCloud() {
   return (
-    <div className="mx-auto max-w-5xl px-6 lg:px-8">
+    <div className="mx-auto  px-6 lg:px-8">
       <div className="mx-auto flex justify-between gap-y-12 lg:max-w-none">
-        <Image
-          src="/img/FirstSolar_logo.svg"
-          alt="FirstSolar logo"
-          width={120}
-          height={140}
-        />
-        <Image
-          src="/img/Sunpower_logo.svg"
-          alt="FirstSolar logo"
-          width={170}
-          height={140}
-        />
-        <Image
-          src="/img/JinkoSolar_logo.svg"
-          alt="FirstSolar logo"
-          width={170}
-          height={140}
-        />
+        <div className="flex flex-col items-center">
+          <Image
+            src="/img/FirstSolar_logo.svg"
+            alt="FirstSolar logo"
+            width={120}
+            height={140}
+          />
+          <span className="text-lg text-gray-500 font-bold">480 watts</span>
+          <span className="text-lg text-gray-500">19%</span>
+        </div>
+        <div className="flex flex-col items-center">
+          <Image
+            src="/img/Sunpower_logo.svg"
+            alt="Sunpower logo"
+            width={170}
+            height={140}
+          />
+          <span className="text-lg text-gray-500 font-bold">440 watts</span>
+          <span className="text-lg text-gray-500">22.8%</span>
+        </div>
+        <div className="flex flex-col items-center">
+          <Image
+            src="/img/JinkoSolar_logo.svg"
+            alt="JinkoSolar logo"
+            width={170}
+            height={140}
+          />
+          <span className="text-lg text-gray-500 font-bold">535 watts</span>
+          <span className="text-lg text-gray-500">21.16%</span>
+        </div>
       </div>
     </div>
   );
