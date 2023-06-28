5fce035a8aff3e331106fc567c31fe05ac02ba05
Hovhannes <hovhannes.mkoyan@gmail.com>
2023-06-28 19:45:51 +0200

    feat: implement filter logic

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -1,9 +1,18 @@
-import { useState } from "react";
+import { Fragment, useState } from "react";
 import Image from "next/image";
 import { useSpring, animated } from "react-spring";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFolderPlus } from "@fortawesome/free-solid-svg-icons";
 import { useQuery } from "@tanstack/react-query";
+import { Listbox, Transition } from "@headlessui/react";
+import { CheckIcon, ChevronUpDownIcon } from "@heroicons/react/20/solid";
+import classnames from "../../utils/classnames";
+
+const filterTypes = [
+  { id: 1, name: "All", value: "all" },
+  { id: 2, name: "Active projects", value: "active" },
+  { id: 3, name: "Old projects", value: "old" },
+];
 
 import SEO from "../../components/SEO";
 import getParsedCookies from "../../utils/cookie-parser";
@@ -12,11 +21,11 @@ import { getProjects } from "../../services/project.service";
 import NewProjectModal from "../../components/UserHomePage/ProjectsPage/NewProjectModal";
 import ProjectSkeleton from "../../components/HelperComponents/Skeleton/ProjectSkeleton";
 import Project from "../../components/UserHomePage/ProjectsPage/Project";
-import FilterDropdown from "../../components/UserHomePage/ProjectsPage/ProjectPage/FilterDropdown";
 
 export default function ProjectsPage(): JSX.Element {
   const [newProjectModalOpen, setNewProjectModalOpen] =
     useState<boolean>(false);
+  const [selected, setSelected] = useState(filterTypes[0]);
 
   const springAnimation = useSpring({
     from: { opacity: 0 },
@@ -49,7 +58,79 @@ export default function ProjectsPage(): JSX.Element {
           </h1>
           {projects?.length! > 0 && (
             <div className="float-right w-[250px] justify-end">
-              <FilterDropdown />
+              <Listbox value={selected} onChange={setSelected}>
+                {({ open }) => (
+                  <>
+                    <div className="relative mt-2">
+                      <Listbox.Button className="sm:text-md relative w-full cursor-default rounded-md bg-white py-2.5 pl-3 pr-10 text-left text-gray-600 shadow-sm ring-1 ring-gray-300 transition hover:shadow-md focus:outline-none sm:leading-6">
+                        <span className="block truncate">{selected?.name}</span>
+                        <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+                          <ChevronUpDownIcon
+                            className="h-5 w-5 text-gray-400"
+                            aria-hidden="true"
+                          />
+                        </span>
+                      </Listbox.Button>
+
+                      <Transition
+                        show={open}
+                        as={Fragment}
+                        leave="transition ease-in duration-100"
+                        leaveFrom="opacity-100"
+                        leaveTo="opacity-0"
+                      >
+                        <Listbox.Options className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg  focus:outline-none sm:text-sm">
+                          {filterTypes.map((person) => (
+                            <Listbox.Option
+                              key={person.id}
+                              className={({ active }) =>
+                                classnames(
+                                  active
+                                    ? "bg-smMain-400 text-white"
+                                    : "text-gray-600",
+                                  "relative cursor-default select-none py-2 pl-3 pr-9"
+                                )
+                              }
+                              value={person}
+                            >
+                              {({ selected, active }) => (
+                                <>
+                                  <span
+                                    className={classnames(
+                                      selected
+                                        ? "font-semibold"
+                                        : "font-normal",
+                                      "block truncate"
+                                    )}
+                                  >
+                                    {person.name}
+                                  </span>
+
+                                  {selected ? (
+                                    <span
+                                      className={classnames(
+                                        active
+                                          ? "text-white"
+                                          : "text-smMain-400",
+                                        "absolute inset-y-0 right-0 flex items-center pr-4"
+                                      )}
+                                    >
+                                      <CheckIcon
+                                        className="h-5 w-5"
+                                        aria-hidden="true"
+                                      />
+                                    </span>
+                                  ) : null}
+                                </>
+                              )}
+                            </Listbox.Option>
+                          ))}
+                        </Listbox.Options>
+                      </Transition>
+                    </div>
+                  </>
+                )}
+              </Listbox>
             </div>
           )}
         </div>
@@ -83,9 +164,22 @@ export default function ProjectsPage(): JSX.Element {
           <>
             <div>
               <animated.div style={springAnimation} className="projects-grid">
-                {projects?.map((project, idx) => (
-                  <Project key={idx} project={project} />
-                ))}
+                {projects
+                  ?.filter((elem) => {
+                    switch (selected?.value) {
+                      case "all":
+                        return true;
+                      case "active":
+                        return !elem.readonly;
+                      case "old":
+                        return elem.readonly;
+                      default:
+                        return false;
+                    }
+                  })
+                  .map((project, idx) => (
+                    <Project key={idx} project={project} />
+                  ))}
 
                 <div
                   className="relative flex min-h-[220px] cursor-pointer flex-col justify-start rounded-md border-2 border-dashed bg-transparent px-5 py-7 shadow-md transition hover:shadow-lg"
