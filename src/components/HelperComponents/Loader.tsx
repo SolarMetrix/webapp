62114139edb61f9774edbcd276c674d09e63f1ac
Hovhannes <hovhannes.mkoyan@gmail.com>
2023-06-30 22:21:11 +0200

    remove unused packages & redundant files

--- src/components/HelperComponents/Loader.tsx
+++ src/components/HelperComponents/Loader.tsx
@@ -3,7 +3,7 @@ import classnames from "../../utils/classnames";
 
 export default function Loader({
   size = 4,
-  color = "text-smMain-500",
+  color = "text-smMain-400",
   classes = "mr-2",
 }: {
   size?: 3 | 4 | 5;
